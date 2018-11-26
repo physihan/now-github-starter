1f663ec6e84cbafb611d0f926e71940d85f2ea76
feixihan <physihan@outlook.com>
2018-11-26 16:50:43 +0800

    feat：创建新的页面

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+
+import   './index.css'
+
+export default function() {
+    return (
+        <div>
+            <ul className="nav">
+                <li>
+                    <a href="./my">我的</a>
+                </li>
+                <li />
+                <li />
+            </ul>
+            哈哈
+        </div>
+    );
+}
